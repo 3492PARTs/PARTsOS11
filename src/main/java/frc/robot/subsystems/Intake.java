d8c13add7e901d8e52bc53982bedc74deef5f530
DanielCurtis1 <dwcurti1@stu.k12.wv.us>
2022-02-05 10:03:14 -0500

    Added toggleIntake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -7,13 +7,24 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 
 public class Intake {  
     TalonSRX intakeMotor = new TalonSRX();
-    
+    Boolean isOn = false;
+
+
     private Intake(){
     
     }
     private static Intake ballIntake = new Intake();
     public static Intake getballIntake () {
         return ballIntake;
+    }
+
+    public void toggleIntake(){
+        if(isOn){
+            setIntakeSpeed(1);
+        } else {
+            setIntakeSpeed(0);
+        }
+        isOn = !isOn;
     }
 
     public void setIntakeSpeed(double speed){
--- src/main/java/frc/robot/subsystems/driverInteraction.java
+++ src/main/java/frc/robot/subsystems/driverInteraction.java
@@ -1,12 +1,14 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class driverInteraction { 
                 Joystick leftJoystick = new Joystick(1);
                 Joystick rightJoystick = new Joystick(0);
                 static driveTrain dTrain = driveTrain.getM_DriveTrain();
                 static driverInteraction dInteraction = new driverInteraction();
+                Intake intake = Intake.getballIntake();
         
         private driverInteraction() {
 
@@ -18,5 +20,14 @@ public class driverInteraction {
 
         public void update() {
                 dTrain.move(leftJoystick.getRawAxis(1), rightJoystick.getRawAxis(1));
+
+                if(leftJoystick.getRawButton(5)){
+                        intake.toggleIntake();           
+                }
+
+        }
+
+        public void intakeInteraction() {
+
         }
 }
