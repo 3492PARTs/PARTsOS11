f7741d9e5db2e60db1c3192bcb2a428ddba28940
Cameron <ckiw23@gmail.com>
2022-03-05 10:54:53 -0500

    fixed shooter errors

--- src/main/java/frc/robot/commands/autoShooting.java
+++ src/main/java/frc/robot/commands/autoShooting.java
@@ -20,7 +20,7 @@ public class autoShooting extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    shooter.getballShooter().setShooterSpeed(speed);
+    Shooter.getballShooter().setShooterSpeed(speed);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
